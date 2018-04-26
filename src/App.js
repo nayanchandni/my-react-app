c6b09999807986b6c76d00163cdde31e6cd35bfd
nayan chandni <nayan.chandni@ge.com>
2018-04-26 10:36:43 -0700

    First attempt at ReactJS

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from './components/headerComponent/header';
+import Footer from './components/footerComponent/footer';
+import Homepage from './components/pages/homePage';
+
+import './Assets/css/default.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+            <Homepage/>
+      </div>
+    );
+  }
+}
+
+export default App;
